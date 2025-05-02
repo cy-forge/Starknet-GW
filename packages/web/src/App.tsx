50aa77cb7c10ce355de2496f95f588c1359c9752
Akshola00 <akinniyishola07@gmail.com>
2025-05-02 23:32:31 +0100

    feat: add profile layout and edit profile modal

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router';
 import './App.css';
 import DashboardLayout from './components/layout/DashboardLayout';
+import ProfileLayout from './components/layout/ProfileLayout';
 import { dashboardRoutes } from './lib/routes';
 import HomePage from './pages/Home';
 function App() {
@@ -11,6 +12,7 @@ function App() {
         {dashboardRoutes.map((route) => (
           <Route key={route.path} path={route.path} element={route.element} />
         ))}
+        <Route path="/profile" element={<ProfileLayout />} />
       </Route>
     </Routes>
   );
